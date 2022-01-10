e50adfad2673e39db4973e87b27fe63e02dff3a6
sandina <alejandro.sandin@roche.com>
2022-01-10 15:46:06 +0100

    Initial commit

---
+++ src/main/java/com/sandin/learning/startingmockito/StartingMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.startingmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandin/learning/startingmockito/StartingMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.startingmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
