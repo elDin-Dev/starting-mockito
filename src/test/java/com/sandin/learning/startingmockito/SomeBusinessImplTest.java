2aa87f3229073133f3d8fb9dfc4666a915cd3dfa
sandina <alejandro.sandin@roche.com>
2022-01-10 16:20:26 +0100

    stub con mockito

--- src/test/java/com/sandin/learning/startingmockito/SomeBusinessImplTest.java
+++ src/test/java/com/sandin/learning/startingmockito/SomeBusinessImplTest.java
@@ -3,12 +3,14 @@ package com.sandin.learning.startingmockito;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SomeBusinessImplTest {
 
 
     @Test
-    public void findTheGreatestFromAllData (){
+    public void testFindTheGreatestFromAllData (){
 
         SomeBusinessImpl business = new SomeBusinessImpl(new DataServiceStub());
 
@@ -17,4 +19,21 @@ public class SomeBusinessImplTest {
         assertEquals(24, result);
 
     }
+
+    @Test
+    public void withMockTestFindTheGreatestFromAllData (){
+
+        DataService dataService = mock(DataService.class);
+        when(dataService.retrieveAllData()).thenReturn(new int[]{32, 6, 15});
+
+
+        SomeBusinessImpl business = new SomeBusinessImpl(dataService);
+
+        int result = business.findTheGreatestFromAllData();
+
+        assertEquals(32, result);
+
+    }
+
+
 }
